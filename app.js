131bf935f1a6eefbee640894b4989bcd56ffbe3d
Lenny <riel_lenny@yahoo.co.uk>
2020-07-30 12:31:42 -0500

    Added mongoose model config

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                  SETUP
+/////////////////////////////////////////////////////////////////////////////
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/recipedex', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(() => console.log("Connected to DB!"))
+.catch(error => console.log(error.message))
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+/////////////////////////////////////////////////////////////////////////////
+//                          MONGOOSE MODEL CONFIG
+/////////////////////////////////////////////////////////////////////////////
+
+// RECIPE SCHEMA
+const recipeScema = new mongoose.Schema({
+    author: String,
+    recipeName: String,
+    body: String,
+    imageURL: String,
+    created: {type: Date, default: Date.now}
+});
+
+// MODEL
+const Recipe = mongoose.model("Recipe", recipeScema);
+
+/////////////////////////////////////////////////////////////////////////////
+//                                  ROUTES
+/////////////////////////////////////////////////////////////////////////////
+
+// INDEX
 app.get('/', (req, res) => {
     res.redirect('/recipes');
 });
