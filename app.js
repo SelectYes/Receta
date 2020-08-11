a5cbe288d06e5c3c22e336234be32140fe5baa34
Lenny <riel_lenny@yahoo.co.uk>
2020-08-11 15:45:09 -0500

    Added user-auth. Must log in to comment/add recip

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 //                                  SETUP
 /////////////////////////////////////////////////////////////////////////////
 
-const express = require('express');
-const app = express();
-const port = 3000;
-const mongoose = require('mongoose');
-const { response } = require('express');
-const methodOverride = require('method-override');
-const Recipe = require('./models/recipe');
-const Comment = require('./models/comment');
-const seedDB = require('./seeds');
+// SERVER/DB
+const express                   = require('express');
+const app                       = express();
+const mongoose                  = require('mongoose');
+const { response }              = require('express');
+const methodOverride            = require('method-override');
+// MODELS
+const Recipe                    = require('./models/recipe');
+const Comment                   = require('./models/comment');
+const User                      = require('./models/user');
+// AUTHENTICATION
+const passport                  = require('passport');
+const localStrategy             = require('passport-local');
+const passportLocalMongoose     = require('passport-local-mongoose')
+const expressSession            = require('express-session');
+// MISC
+const seedDB                    = require('./seeds');
+const port                      = 3000;
 
 mongoose.connect('mongodb://localhost:27017/recipedex', {
     useNewUrlParser: true,
@@ -24,6 +33,37 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+
+/////////////////////////////////////////////////////////////////////////////
+//                           AUTHENTICATION CONFIG
+/////////////////////////////////////////////////////////////////////////////
+
+app.use(expressSession({
+    secret: "Benny is the cutest and best dog in the entire universe",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// config for using 'authenticate()' as middleware
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
 
 /////////////////////////////////////////////////////////////////////////////
 //                             RECIPE ROUTES
@@ -54,12 +94,12 @@ app.get('/recipes', (req, res) => {
 });
 
 // NEW
-app.get('/recipes/new', (req, res) => {
+app.get('/recipes/new', isLoggedIn, (req, res) => {
     res.render('recipes/new');
 });
 
 // CREATE
-app.post('/recipes', (req, res) => {
+app.post('/recipes', isLoggedIn, (req, res) => {
     Recipe.create(req.body.recipe, (err, newRecipe) => {
         if (err) {
             console.log(err);
@@ -119,7 +159,7 @@ app.delete('/recipes/:id', (req, res) => {
 /////////////////////////////////////////////////////////////////////////////
 
 // NEW ROUTE
-app.get('/recipes/:id/comments/new', async (req, res) => {
+app.get('/recipes/:id/comments/new', isLoggedIn, async (req, res) => {
     try {
         const recipe = await Recipe.findById(req.params.id);
         res.render('comments/new', {recipe: recipe});
@@ -129,7 +169,7 @@ app.get('/recipes/:id/comments/new', async (req, res) => {
 });
 
 // CREATE ROUTE
-app.post('/recipes/:id/comments', async (req, res) => {
+app.post('/recipes/:id/comments', isLoggedIn, async (req, res) => {
     try {
         const recipe = await Recipe.findById(req.params.id);
         const comment = await Comment.create(req.body.comment);
@@ -142,6 +182,53 @@ app.post('/recipes/:id/comments', async (req, res) => {
     } catch (error) {
         console.log(error);
     }
+});
+
+/////////////////////////////////////////////////////////////////////////////
+//                             AUTH ROUTES
+/////////////////////////////////////////////////////////////////////////////
+
+// SIGN UP ROUTES
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const newUser = new User({ username: req.body.username });
+        const password = req.body.password;
+    
+        await User.register(newUser, password)
+
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/recipes');
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.redirect('/register');
+    }
+});
+
+// SIGN-IN ROUTES
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', 
+    {
+        successRedirect: '/recipes',
+        failureRedirect: '/login'
+    }), (req, res) => {
+});
+
+// SIGN OUT ROUTE
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/recipes');   
 });
 
 app.listen(port, () => console.log(`Serving RecipeDex on localhost:${port}`))
